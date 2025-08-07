ed86db7dd06d3858cc0e9d51361f757f7d871630
Boris Polak <18208654+BorisTB@users.noreply.github.com>
2025-08-08 00:47:49 +0200

    chore(repo): update vulnerable dependencies

--- modules/course/components/VideoPlayer/VideoPlayer.tsx
+++ modules/course/components/VideoPlayer/VideoPlayer.tsx
@@ -1,12 +1,16 @@
-import ReactPlayer from "react-player";
-import styles from './VideoPlayer.module.scss'
+import ReactPlayer from 'react-player';
+import styles from './VideoPlayer.module.scss';
 
 type Props = {
   url: string;
 };
 
 const VideoPlayer: React.FC<Props> = ({ url }) => {
-  return <div className={styles['video-player']}> <ReactPlayer url={url} controls width='100%' height="100%" /></div>;
+  return (
+    <div className={styles['video-player']}>
+      <ReactPlayer src={url} controls width="100%" height="100%" />
+    </div>
+  );
 };
 
 export default VideoPlayer;
