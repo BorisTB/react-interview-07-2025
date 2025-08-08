c6eabd8d18857a4ef1ddbc348eddb8c50e71cebf
Boris Polak <18208654+BorisTB@users.noreply.github.com>
2025-08-08 03:27:24 +0200

    fix(course): tag video as completed when it ends
    - ! We don't check if the video was actually watched

--- modules/course/components/VideoPlayer/VideoPlayer.tsx
+++ modules/course/components/VideoPlayer/VideoPlayer.tsx
@@ -3,12 +3,20 @@ import styles from './VideoPlayer.module.scss';
 
 type Props = {
   url: string;
+  /** Called when media finishes playing **/
+  onEnded?: () => void;
 };
 
-const VideoPlayer: React.FC<Props> = ({ url }) => {
+const VideoPlayer: React.FC<Props> = ({ url, onEnded }) => {
   return (
     <div className={styles['video-player']}>
-      <ReactPlayer src={url} controls width="100%" height="100%" />
+      <ReactPlayer
+        src={url}
+        onEnded={onEnded}
+        controls
+        width="100%"
+        height="100%"
+      />
     </div>
   );
 };
